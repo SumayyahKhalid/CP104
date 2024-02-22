0f63533f157df7a4af31ed2b7791b31613c6e0cd
SumayyahKhalid <152665748+SumayyahKhalid@users.noreply.github.com>
2024-02-22 15:32:17 -0500

    Add files via upload

---
+++ L08/t05.py
@@ -0,0 +1,14 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author: Sumayyah Khalid
+ID: 169022335
+Email: khal2335@mylaurier.ca
+__updated__ = "2022-10-22"
+-------------------------------------------------------
+"""
+from functions import is_leap
+print(is_leap(1000))
+print(is_leap(1800))
+print(is_leap(2020))
